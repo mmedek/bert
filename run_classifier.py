84f0c5859991c94328a1067f0ade8d0ed6b5adc0
Michal Medek <michal.medek@socialbakers.com>
2018-11-13 00:20:31 +0100

    changed loading csv to tsv

--- run_classifier.py
+++ run_classifier.py
@@ -279,18 +279,18 @@ class SentimentProcessor(DataProcessor):
   def get_train_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "train.csv")), "train")
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev.csv")),
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")),
         "dev_matched")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test.csv")), "test")
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
   def get_labels(self):
     """See base class."""
